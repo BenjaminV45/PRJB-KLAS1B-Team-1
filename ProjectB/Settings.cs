19343192f89864c101da585bdf67acad3c83e189
BenjaminV45 <45715664+BenjaminV45@users.noreply.github.com>
2021-03-28 13:06:54 +0200

    Garbage

--- ProjectB/Settings.cs
+++ ProjectB/Settings.cs
@@ -7,14 +7,12 @@ namespace ProjectB
     public class Settings
     {
         public static JObject SETTINGS = Newtonsoft.Json.JsonConvert.DeserializeObject(File.ReadAllText("settings.json")) as JObject;
-        public static string SETTING_LANG = (string)SETTINGS["language"];
 
         public string getText(object[] value)
         {
-            JObject language = Newtonsoft.Json.JsonConvert.DeserializeObject(File.ReadAllText("language.json")) as JObject;
-            string data = (string)language[SETTING_LANG][value[0]][value[1]];
+            JObject language = Newtonsoft.Json.JsonConvert.DeserializeObject(File.ReadAllText("languages.json")) as JObject;
+            string data = (string)language[(string)SETTINGS["language"]][value[0]][value[1]];
             return data;
         }
     }
-
 }
