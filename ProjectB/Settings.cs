cd8739816c57360f82a3aa7b965d7ce7c02f02b8
BenjaminV45 <45715664+BenjaminV45@users.noreply.github.com>
2021-03-29 21:46:17 +0200

    Test

--- ProjectB/Settings.cs
+++ ProjectB/Settings.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.IO;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace ProjectB
 {
     public class Settings
     {
         public static JObject SETTINGS = Newtonsoft.Json.JsonConvert.DeserializeObject(File.ReadAllText(@"..\..\..\Data\settings.json")) as JObject;
+        
+        //static int LogKey(string args) 
+        //{
+        //    List<string> log = new List<string>();
+        //    log.Add(args);
+        //    return log.Count;
 
+        //}
         public string getText(object[] value)
         {
             JObject language = Newtonsoft.Json.JsonConvert.DeserializeObject(File.ReadAllText(@"..\..\..\Data\languages.json")) as JObject;
