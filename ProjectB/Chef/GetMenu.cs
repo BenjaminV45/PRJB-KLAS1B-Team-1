2faeba0198a64f6700b2ff1935fde912bb391106
BenjaminV45 <45715664+BenjaminV45@users.noreply.github.com>
2021-04-22 13:58:13 +0200

    Het betere leven

---
+++ ProjectB/Chef/GetMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectB
+{
+    class GetMenu
+    {
+        public GetMenu()
+        {
+            Console.WriteLine("GetMenu werkt");
+        }
+    }
+}
--- ProjectB/Program.cs
+++ ProjectB/Program.cs
@@ -1,20 +1,89 @@
 ﻿using System;
-using System.IO;
-using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace ProjectB
 {
-    class Program
+    class System
     {
 
-        static void Main(string[] args)
+    }
+
+    class Json
+    {
+
+    }
+
+    class Option : System
+    {
+        public void Menu(Tuple<int, string, Action>[] options)
+        {
+            foreach (Tuple<int, string, Action> row in options)
+            {
+                Console.WriteLine($"[{row.Item1}] {row.Item2}");
+            }
+
+            int input;
+            bool tmp = false;
+            while (!tmp)
+            {
+
+                try
+                {
+                    Console.Write("\nPick a number: ");
+                    input = Convert.ToInt32(Console.ReadLine());
+                    if (input > options.Length || input < 1)
+                    {
+                        Console.WriteLine("** " + input + " does not exist. **");
+                    }
+                    else
+                    {
+                        tmp = true;
+                        options[input - 1].Item3();
+                    }
+                }
+
+                catch
+                {
+                    Console.WriteLine("** Input is not valid. Please pick a number. **");
+                }
+            }
+        }
+    }
+    class Chef : Option
+    {
+        public Chef()
+        {
+            var options = new[]
+            {
+                Tuple.Create<int, string, Action>(1, "Krijg menu", () => new GetMenu()),
+
+            };
+            this.Menu(options);
+        }
+    }
+
+    class Customer : Option
+    {
+
+    }
+
+    class Start : Option
+    {
+
+        public Start()
         {
 
-            Start Start = new Start();
-            Start.Construct();
+        }
+    }
 
-            Options Options = new Options();
-            Options.options();
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Chef test = new Chef();
         }
     }
 }
