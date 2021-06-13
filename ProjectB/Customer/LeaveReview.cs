bf784c5f39ff0d2d60192c2b699125b86719766e
mrcocchieman <salar.ali@live.nl>
2021-06-13 20:43:09 +0200

    Laatste check

--- ProjectB/Customer/LeaveReview.cs
+++ ProjectB/Customer/LeaveReview.cs
@@ -14,16 +14,14 @@ namespace ProjectB
         public bool woorden;
         public leaveReview()
         {
-            this.korting = false;
-            string[] kortingarr = new string[] { "fantastisch", "geweldig", "heerlijk", "prachtig" };
-            this.woorden = false;
-            string[] woordenarr = new string[] { "mieters", "super", "vet", "top" };
-
             dynamic reviews = new Json("reviews.json").Read();
             dynamic reservation = new Json("reservation.json").Read();
             dynamic members = new Json("members.json").Read();
 
-
+            this.korting = false;
+            string[] kortingarr = new string[] { "fantastisch", "geweldig", "heerlijk", "prachtig" };
+            this.woorden = false;
+            string[] woordenarr = new string[] { "mieters", "super", "vet", "top" };
 
             bool tmp = false;
             while (!tmp)
@@ -57,12 +55,9 @@ namespace ProjectB
                 }
                 if (!tmp5)
                 {
-                    Console.WriteLine("wrong input");
+                    new Alfred("leaveReview", 10).Write();
                 }
             }
-
-
-
 
             new Alfred("leaveReview", 1).Write();
             this.review = Console.ReadLine();
@@ -119,15 +114,6 @@ namespace ProjectB
                  id = 1;
             }
 
-            var newreview = new ReviewsJson
-            {
-                id = id,
-                resid = this.resid,
-                memberid = new Settings().Member_id,
-                reviewtxt = this.review,
-                rating = this.sterren
-            };
-
             if (this.korting && this.woorden)
             {
                 new Alfred("leaveReview", 8).Write();
@@ -144,6 +130,15 @@ namespace ProjectB
             {
                 new Alfred("leaveReview", 5).Write();
             }
+
+            var newreview = new ReviewsJson
+            {
+                id = id,
+                resid = this.resid,
+                memberid = new Settings().Member_id,
+                reviewtxt = this.review,
+                rating = this.sterren
+            };
 
             reviews.Add(newreview);
             new Json("reviews.json").Write(reviews);
